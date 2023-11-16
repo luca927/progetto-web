d403836ddbc60e78bec1eea2dc069286b52808b2
luca927 <146101549+luca927@users.noreply.github.com>
2023-11-16 21:03:59 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'orizontal',
+    loop: true,
+  
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar'},
+  });
